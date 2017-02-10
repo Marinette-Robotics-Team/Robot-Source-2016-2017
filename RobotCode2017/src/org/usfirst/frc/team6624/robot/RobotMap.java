6f8213bd2dd83f18ea28f921a949bd58595a2eeb
macuser47 <fred-simons47@hotmail.com>
2017-02-09 19:50:04 -0600

    Updated encoder ports to match board
    See title.

--- RobotCode2017/src/org/usfirst/frc/team6624/robot/RobotMap.java
+++ RobotCode2017/src/org/usfirst/frc/team6624/robot/RobotMap.java
@@ -37,8 +37,8 @@ public class RobotMap {
 	public static int rangeFinderPort = 0;
 	
 	//DIO ports
-	public static int leftEncoderChannelA = 0;
-	public static int leftEncoderChannelB = 1;
-	public static int rightEncoderChannelA = 2;
-	public static int rightEncoderChannelB = 3;
+	public static int rightEncoderChannelA = 0;
+	public static int rightEncoderChannelB = 1;
+	public static int leftEncoderChannelA = 2;
+	public static int leftEncoderChannelB = 3;
 }
