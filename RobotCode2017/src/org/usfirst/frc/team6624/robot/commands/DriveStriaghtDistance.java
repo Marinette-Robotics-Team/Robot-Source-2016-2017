72095d5b4dd32a830511062151b0bd38e3be011c
macuser47 <fred-simons47@hotmail.com>
2017-02-13 16:16:50 -0600

    Revert "Updated DriveStriaghtDistance math for minimum speed."
    This reverts commit c290ab3b59d930a2658e1fe830c6d5293f68b298.

--- RobotCode2017/src/org/usfirst/frc/team6624/robot/commands/DriveStriaghtDistance.java
+++ RobotCode2017/src/org/usfirst/frc/team6624/robot/commands/DriveStriaghtDistance.java
@@ -119,8 +119,8 @@ public class DriveStriaghtDistance extends Command {
     	timer.start();
     	
     	//set timer times
-    	tAccelerating = (maxVelocity - MIN_SPEED) / acceleration;
-    	tCruising = (distance - ((Math.pow(maxVelocity - MIN_SPEED, 2) / acceleration) + (2 * (maxVelocity - MIN_SPEED) / acceleration))) / maxVelocity;
+    	tAccelerating = maxVelocity / acceleration;
+    	tCruising = (distance - (Math.pow(maxVelocity, 2) / acceleration)) / maxVelocity;
     	tTotal = 2 * tAccelerating  + tCruising;
     	
     	
